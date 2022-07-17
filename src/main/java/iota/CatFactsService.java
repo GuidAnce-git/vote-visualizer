58682e2b7fa2cbafe2c3a16ca630e651aad1ca86
GuidAnce <robert@zeiger.club>
2022-07-18 00:17:36 +0200

    test API added

--- src/main/java/iota/CatFactsService.java
+++ src/main/java/iota/CatFactsService.java
@@ -4,12 +4,14 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
 
 @RegisterRestClient(configKey="catFacts-api")
 @Singleton
 public interface CatFactsService {
 
     @GET
+    @Produces("application/json")
     CatFactsDO getFacts();
     
 }
