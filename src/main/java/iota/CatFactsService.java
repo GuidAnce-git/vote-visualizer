4c06008145310c9c3b5fb1e7b60527235ed95a3b
GuidAnce <robert@zeiger.club>
2022-07-18 00:09:06 +0200

    test API added

--- src/main/java/iota/CatFactsService.java
+++ src/main/java/iota/CatFactsService.java
@@ -2,9 +2,11 @@ package iota;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 
 @RegisterRestClient(configKey="catFacts-api")
+@Singleton
 public interface CatFactsService {
 
     @GET
