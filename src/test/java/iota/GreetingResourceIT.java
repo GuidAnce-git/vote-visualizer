014bc11e176e6ffba3e34b677c2ca28ea7edf463
GuidAnce <robert@zeiger.club>
2022-07-17 22:46:05 +0200

    Add a Procfile

---
+++ src/test/java/iota/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package iota;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
